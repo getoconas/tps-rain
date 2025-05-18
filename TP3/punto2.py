8438bfbe251ab53daf03825d53f113eef4d31672
getoconas <getoconas@gmail.com>
2025-05-17 21:03:01 -0300

    s2 tp3 | extract image of the news

--- TP3/punto2.py
+++ TP3/punto2.py
@@ -1,3 +1,4 @@
+import re # re es una biblioteca para trabajar con expresiones regulares
 import requests # requests es una biblioteca para realizar solicitudes HTTP
 from bs4 import BeautifulSoup # BeautifulSoup es una biblioteca para analizar documentos HTML y XML
 from unidecode import unidecode # unidecode es una biblioteca para convertir caracteres Unicode a ASCII
@@ -19,23 +20,45 @@ def get_details_news(url):
     # Titulo de la noticia
     title_element_news = soup.find('h1', class_='display-block article-headline text_align_left').text.strip() 
     title_news = unidecode(title_element_news).lower() if title_element_news else "*** No se encontro titulo de la noticia ***" # Convertir el título a ASCII, eliminar acentos y convertir a minúsculas
-    print('*** Titulo noticia: ***', title_news)
+    #print('*** Titulo noticia: ***', title_news)
 
     # Resumen de la noticia
     summary_element_news = soup.find('h2', class_='article-subheadline text_align_left').text.strip()
     summary_news = unidecode(summary_element_news).lower() if summary_element_news else "*** No se encontro resumen de la noticia ***"
-    print('*** Resumen noticia: ***', summary_news)
+    #print('*** Resumen noticia: ***', summary_news)
 
     # Contenido de la noticia
     content_element_news = soup.find('div', class_='body-article')
-    #content_news = unidecode(content_element_news).lower() if content_element_news else "*** No se encontro contenido de la noticia ***"
-    #print('*** Contenido noticia: ***', content_element_news)
+
+    if content_element_news:
+      content = content_element_news.find_all(['p', 'h2'])
+      content_news = [unidecode(c.get_text(separator=" ", strip=True)).lower() for c in content]
+      #print (content_news)
+
+    # Imagenes dentro de la noticia
+    images_news = []
+    if content_element_news:
+      images_element_news = content_element_news.find_all('img')
+      for img in images_element_news:
+        img_url = img.get('src')
+        if img_url:
+          images_news.append(img_url)
+    
+    #print('*** Imagenes noticia: ***', images_news)
+
+    # Eliminar signos de puntuacion del titulo, resumen y contenido
+    title_news = re.sub(r'[^\w\s]', '', title_news)
+    summary_news = re.sub(r'[^\w\s]', '', summary_news)
+    content_news = [re.sub(r'[^\w\s]', '', c) for c in content_news]
+    content_total_news = ""
+    for i in range(len(content_news)):
+      content_total_news += content_news[i] + " "
 
     print("------------------------------------------------------------------------------------------------------------")
-    if content_element_news:
-      content_news = content_element_news.find_all(['p', 'h2'])
-      content = [unidecode(c.get_text(separator=" ", strip=True)).lower() for c in content_news]
-      print (content)
+    print('*** Titulo noticia: ***', title_news)
+    print('*** Resumen noticia: ***', summary_news)
+    print('*** Contenido noticia: ***', content_total_news)
+
 
 # Programa principal
 # Realizar la solicitud HTTP a la página web
