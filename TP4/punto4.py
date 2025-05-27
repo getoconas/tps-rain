ad6293c2e111869fbaf63007083585f8df982143
getoconas <getoconas@gmail.com>
2025-05-27 20:33:01 -0300

    s4 tp4 | solution to extract content for csv

--- TP4/punto4.py
+++ TP4/punto4.py
@@ -13,15 +13,16 @@ data = []
 count = 0
 
 print("Iniciando la recolección...")
-while count < 100:
+while count < 100: # Limitar a 100 registros
   try:
     record = next(iterator) 
     metadata = record.metadata # Obtener los metadatos del registro
-    title = metadata.get('title', [''])[0]
-    description = metadata.get('description', [''])[0]
-    author = '; '.join(metadata.get('creator', []))
-    keywords = '; '.join(metadata.get('subject', []))
-    date = metadata.get('date', [''])[0]
+    
+    title = '; '.join(metadata.get('title', [])) # Obtener el título del registro
+    description = '; '.join(metadata.get('description', [])) # Obtener la descripción del registro
+    author = '; '.join(metadata.get('creator', [])) # Obtener los autores del registro
+    keywords = '; '.join(metadata.get('subject', [])) # Obtener las palabras clave del registro
+    date = '; '.join(metadata.get('date', [])) # Obtener la fecha del registro
 
     data.append(
       {
@@ -34,7 +35,7 @@ while count < 100:
     )
     count += 1
     print(f" Se agregaron {count} registros")
-  
+
   except StopIteration:
     print("No hay más registros disponibles.")
     break
@@ -42,10 +43,10 @@ while count < 100:
   except Exception as e:
     print(f"Registro omitido por un error")
     continue
- 
+
 # Guardar en CSV
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, columns = ['title', 'description', 'author', 'keywords', 'date'])
 filename = f'metadata_cora_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv'
-df.to_csv(filename, index=False, encoding='utf-8-sig')
+df.to_csv(filename, index = False, encoding = 'utf-8-sig', sep = ';')
 
 print(f"Archivo CSV generado con {count} registros en {filename}")
